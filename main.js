acbac22893fb8c08fb0105a7d99bb0420ae0f37b
Miles <mpomeroy@lingotek.com>
2014-04-30 20:35:14 -0600

    Initial commit.

---
+++ main.js
@@ -0,0 +1,184 @@
+
+var data = [
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00001.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00002.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00003.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00004.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00005.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00006.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00007.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00008.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00009.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00010.jpg'},
+  {year: 2004, url: 'https://s3.amazonaws.com/milesandmichelle/game/2004-00011.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00001.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00002.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00003.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00004.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00005.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00006.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00007.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00008.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00009.jpg'},
+  {year: 2005, url: 'https://s3.amazonaws.com/milesandmichelle/game/2005-00010.jpg'},
+  {year: 2006, url: 'https://s3.amazonaws.com/milesandmichelle/game/2006-00001.jpg'},
+  {year: 2006, url: 'https://s3.amazonaws.com/milesandmichelle/game/2006-00002.jpg'},
+  {year: 2006, url: 'https://s3.amazonaws.com/milesandmichelle/game/2006-00003.jpg'},
+  {year: 2006, url: 'https://s3.amazonaws.com/milesandmichelle/game/2006-00004.jpg'},
+  {year: 2006, url: 'https://s3.amazonaws.com/milesandmichelle/game/2006-00005.jpg'},
+  {year: 2006, url: 'https://s3.amazonaws.com/milesandmichelle/game/2006-00006.jpg'},
+  {year: 2006, url: 'https://s3.amazonaws.com/milesandmichelle/game/2006-00007.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00001.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00002.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00003.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00004.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00005.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00006.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00007.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00008.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00009.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00010.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00011.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00012.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00013.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00014.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00015.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00016.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00017.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00018.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00019.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00020.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00021.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00022.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00023.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00024.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00025.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00026.jpg'},
+  {year: 2007, url: 'https://s3.amazonaws.com/milesandmichelle/game/2007-00027.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00001.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00002.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00003.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00004.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00005.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00006.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00007.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00008.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00009.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00010.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00011.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00012.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00013.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00014.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00015.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00016.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00017.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00018.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00019.jpg'},
+  {year: 2008, url: 'https://s3.amazonaws.com/milesandmichelle/game/2008-00020.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00001.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00002.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00003.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00004.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00005.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00006.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00007.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00008.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00009.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00010.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00011.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00012.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00013.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00014.jpg'},
+  {year: 2009, url: 'https://s3.amazonaws.com/milesandmichelle/game/2009-00015.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00001.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00002.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00003.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00004.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00005.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00006.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00007.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00008.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00009.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00010.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00011.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00012.jpg'},
+  {year: 2010, url: 'https://s3.amazonaws.com/milesandmichelle/game/2010-00013.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00001.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00002.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00003.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00004.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00005.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00006.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00007.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00008.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00009.jpg'},
+  {year: 2011, url: 'https://s3.amazonaws.com/milesandmichelle/game/2011-00010.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00001.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00002.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00003.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00004.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00005.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00006.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00007.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00008.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00009.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00010.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00011.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00012.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00013.jpg'},
+  {year: 2012, url: 'https://s3.amazonaws.com/milesandmichelle/game/2012-00014.jpg'}
+];
+
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomImage() {
+  var rand = getRandomInt(0, data.length - 1);
+  return data.splice(rand, 1)[0];
+}
+
+var currentImage = getRandomImage();
+var score = 0;
+var total = 1;
+
+$(function () {
+  var image = $('img', '#current-image').attr('src', currentImage.url);
+
+  $('button', '#years').click(function () {
+    var guess = $(this).text();
+    if(guess == currentImage.year) {
+      score++;
+      $('#grade').text('Correct').addClass('correct').removeClass('wrong');
+    } else {
+      $('#grade').text('Wrong').removeClass('correct').addClass('wrong');
+    }
+
+    $('#correct-answer').text(currentImage.year);
+    $('#current-score').text(score + '/' + total);
+
+    $('#years').hide();
+    $('#reveal').show();
+
+    if(data.length === 0) {
+      $('.pager').hide();
+      $('#game-over').show();
+    }
+  });
+
+  $('#next').click(function() {
+    currentImage = getRandomImage();
+    image
+      .attr('src', '')
+      .attr('src', currentImage.url);
+
+    total++;
+
+    $('#years').show();
+    $('#reveal').hide();
+  });
+});
+
+
+
+
